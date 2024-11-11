430990b6b555014690bdba49f9696ff0b13ad831
Ryan Bruels <ryan@atypicalai.com>
2024-11-11 12:12:41 -0800

    Add json field to Media

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -11,6 +11,11 @@ export const Media: CollectionConfig = {
       type: 'text',
       required: true,
     },
+    {
+      name: 'data',
+      type: 'json',
+      required: true,
+    }
   ],
   upload: true,
 }
